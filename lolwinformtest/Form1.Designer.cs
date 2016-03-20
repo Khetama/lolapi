586117255dbbebd4ee6465a3c438a388d13d7690
Khetama <alexandre.goldstein@laposte.net>
2016-03-20 20:53:40 +0100

    first version of my lol api

---
+++ lolwinformtest/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace lolwinformtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.summonerTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.gameGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gameGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // summonerTextBox
+            // 
+            this.summonerTextBox.Location = new System.Drawing.Point(23, 42);
+            this.summonerTextBox.Name = "summonerTextBox";
+            this.summonerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.summonerTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(23, 68);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 32);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // gameGridView
+            // 
+            this.gameGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gameGridView.Location = new System.Drawing.Point(23, 106);
+            this.gameGridView.Name = "gameGridView";
+            this.gameGridView.Size = new System.Drawing.Size(492, 264);
+            this.gameGridView.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 465);
+            this.Controls.Add(this.gameGridView);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.summonerTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gameGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox summonerTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView gameGridView;
+    }
+}
+
---
+++ lolwinformtest/Form1.cs
@@ -0,0 +1,52 @@
+﻿using RiotSharp;
+using RiotSharp.SummonerEndpoint;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lolwinformtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+         
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            var api = RiotApi.GetInstance("d254864f-4fda-4807-8e01-6e44b2349ad7");
+            Summoner summoner;
+            RiotSharp.Region region = RiotSharp.Region.euw;
+
+            try
+            {
+                summoner = api.GetSummoner(region, summonerTextBox.Text);
+                //MessageBox.Show(string.Format("Id : {0} {1}", summoner.Id, "toto"));
+
+                //var matchList = api.GetMatchList(region, summoner.Id, null, );
+                var gameList = api.GetRecentGames(region, summoner.Id);
+                var masteries = api.GetMasteryPages(region, summoner.Id);
+
+                gameGridView.DataSource = gameList;
+
+
+            }
+            catch (RiotSharpException ex)
+            {
+                MessageBox.Show(ex.Message);
+                // Handle the exception however you want.
+            }
+
+
+        }
+    }
+}
